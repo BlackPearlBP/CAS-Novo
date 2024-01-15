2462c3e0ed56e7913281ccfc6bde629a9281bd7c
Gabriel L Soares <gabri.soares007@gmail.com>
2024-01-15 14:38:49 -0300

    Update Admin.java

--- CAS/src/Admin.java
+++ CAS/src/Admin.java
@@ -6,15 +6,24 @@ public class Admin {
     
     public boolean verificarAdmin(String usuarioAdmin, String senhaAdmin) {
         if((usuario.equals(usuarioAdmin)) && (senha.equals(senhaAdmin))){
-                System.out.println("        Entrou como administrador");
+                System.out.println("\n======================================================================================\n" +  
+                    "|        Entrou como administrador                                                   |" + 
+                    "\n======================================================================================\n");
                 return true;
         }else{
-                System.out.println("        Não foi possivel entrar como administrador");
+                System.out.println("\n======================================================================================\n" + 
+                    "|        Não foi possivel entrar como administrador                                  |" + 
+                    "\n======================================================================================\n");
                 return false;
         }
     }       
         public static int menuAdmin() {
-        System.out.println("        Escolha uma opção:\n        [1] - Alterar descontos\n        [2] - Alterar login administrador\n        [3] - Voltar");
+        System.out.println("\n======================================================================================\n" + 
+            "|        Escolha uma opção:                                                                  |\n" + 
+            "|        [1] - Alterar descontos                                                             |\n" + 
+            "|        [2] - Alterar login administrador                                                   |\n" + 
+            "|        [3] - Voltar                                                                        |" + 
+            "\n======================================================================================\n");
         Scanner entradaAdmin = new Scanner(System.in);
         int entrada = entradaAdmin.nextInt();
         return entrada;
